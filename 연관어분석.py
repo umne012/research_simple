9043f00d6d870a89ffe9d7547b99dc5002baf759
umne <inakame@naver.com>
2025-04-03 23:42:56 +0900

    Update 연관어분석.py

--- 연관어분석.py
+++ 연관어분석.py
@@ -111,7 +111,7 @@ def show_relation_tab():
     const svg = d3.select("svg");
     
     const simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(d => d.id).distance(50))
+        .force("link", d3.forceLink(links).id(d => d.id).distance(80))
         .force("charge", d3.forceManyBody().strength(-100))
         .force("center", d3.forceCenter(width / 2, height / 2));
         
